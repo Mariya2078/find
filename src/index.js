b56a3e1db2fa05c7735a8d0f69f7edfc40c637c4
Mariya Butova <yugkarton2015@ukr.net>
2021-05-12 22:40:27 +0300

    new

---
+++ src/index.js
@@ -0,0 +1,37 @@
+
+"use strict";
+
+const a = 55;
+const b = 2;
+const x = a + b;
+// console.log(x);
+// console.log("hello");
+// //alert(1234);
+// alert("привет Маруся");
+function hello() {
+    console.log("ПРИВЕТ ВСЕМ");
+}
+hello();
+//const age = 15;
+//console.log(age);
+let g = 1;
+console.log(g);
+ g = 2;
+console.log(g);
+
+console.log(2 > 1);
+console.log(typeof g);//TYPE
+
+//const goToWork = confirm("ИДЕМ НА РАБОТУ");
+//console.log(goToWork);//true or false
+
+const age = prompt("how old are you");
+console.log(age);// Вввод данных в окошко
+
+if (age >= 18)
+{
+    console.log("You can buy this");
+    
+} else {
+    console.log("Where is your parents???");
+    }
